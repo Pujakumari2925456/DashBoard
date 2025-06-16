54d51d07dc2a60fc2c0bc4ac54db637037a66c5a
Pujakumari2925456 <pujakumari456pandey@gmail.com>
2025-06-16 15:29:49 +0530

    first screen

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,12 +1,34 @@
-import { Box } from "@mui/material";
+import React, { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
 
 const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  // Dummy user data (can later be fetched from API or global state)
+  const user = {
+    name: "Demo User",
+    occupation: "Developer",
+  };
+
   return (
-    <Box width="100%" height="100%">
-      <Box>
-        <Navbar />
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        user={user}
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box flexGrow={1}>
+        <Navbar
+          user={user}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
         <Outlet />
       </Box>
     </Box>
@@ -14,3 +36,4 @@ const Layout = () => {
 };
 
 export default Layout;
+
